0daea49144cbf42c5f749b3db3d4c361cd2693c6
Adam-Batiuk <159185155+Adam-Batiuk@users.noreply.github.com>
2024-03-21 18:21:03 +0000

    done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Mr. Adam Batiuk All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Adam Batiuk
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+"use strict"
+
+function findAreaOfTriangle() {
+  // Define the lengths of the base and hight of a triangle
+  const baseOfTriangle = parseInt(
+    document.getElementById("base-of-triangle").value
+  )
+  const hightOfTriangle = parseInt(
+    document.getElementById("hight-of-triangle").value
+  )
+  
+  //Calculate the area of the triangle
+  const areaOfTriangle = (baseOfTriangle * hightOfTriangle) / 2
+  const roundedArea = Math.round(areaOfTriangle * 100) / 100
+
+  document.getElementById("answer").innerHTML =
+    "Area is: " + roundedArea + " cm²"
+}
